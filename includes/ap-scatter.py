5d44a758e3a899569e036f38d057002bcf6c558f
xtr4nge <xtr4nge@gmail.com>
2016-10-08 11:02:08 +0100

    mode probe request has been added

--- includes/ap-scatter.py
+++ includes/ap-scatter.py
@@ -5,7 +5,6 @@ from scapy.all import *
 import getopt
 import time
 
-# spoof-ssid
 # ------- MENU -------
 def usage():
     print "\nap-scatter 1.0 by xtr4nge"
@@ -17,6 +16,7 @@ def usage():
     print "-s <filter-station>                      station filter (macaddress)"
     print "-b <bssid>                               Rogue AP (bssid)"
     print "-e <filter>                              Filter ESSID (none, whitelist, blacklist)"
+    print "-m <num>                                 Mode Broadcast=1 | Request=2 (use mode 2 with Mana Loud)"
     print "-h                                       Print this help message."
     print ""
     print "Author: xtr4nge"
@@ -29,10 +29,11 @@ def parseOptions(argv):
     FILTER_STATION = ""
     ROGUE_BSSID = "00:00:00:00:00:01"
     FILTER_SSID = "none"
+    MODE = 1
 
     try:
-        opts, args = getopt.getopt(argv, "hi:t:l:b:s:e:",
-                                   ["help", "interface=", "time=", "log=", "bssid=", "station=", "essid="])
+        opts, args = getopt.getopt(argv, "hi:t:l:b:s:e:m:",
+                                   ["help", "interface=", "time=", "log=", "bssid=", "station=", "essid=", "mode="])
 
         for opt, arg in opts:
             if opt in ("-h", "--help"):
@@ -52,8 +53,10 @@ def parseOptions(argv):
                 FILTER_STATION = arg
             elif opt in ("-e", "--essid"):
                 FILTER_SSID = arg
+            elif opt in ("-m", "--mode"):
+                MODE = int(arg)
                 
-        return (INTERFACE, TIME, LOG, ROGUE_BSSID, FILTER_STATION, FILTER_SSID)
+        return (INTERFACE, TIME, LOG, ROGUE_BSSID, FILTER_STATION, FILTER_SSID, MODE)
                     
     except getopt.GetoptError:           
         usage()
@@ -65,7 +68,7 @@ def parseOptions(argv):
 SSIDS = []
 BCAST = "ff:ff:ff:ff:ff:ff"
 
-(INTERFACE, TIME, LOG, ROGUE_BSSID, FILTER_STATION, FILTER_SSID) = parseOptions(sys.argv[1:])
+(INTERFACE, TIME, LOG, ROGUE_BSSID, FILTER_STATION, FILTER_SSID, MODE) = parseOptions(sys.argv[1:])
 
 def loadFILTER():
     FILTER =[]
@@ -109,28 +112,28 @@ while True:
     SSIDS = loadSSID(FILTER_SSID)
     
     for ESSID in SSIDS:
-        #BSSID = RandMAC()
         
-        print TARGET, ROGUE_BSSID, ESSID
+        if MODE == 1:
+        	
+            # BROADCAST
+            print '[*] 802.11 Beacon: SSID=[%s], BSSID=%s, TARGET=%s' % (ESSID, ROGUE_BSSID, TARGET)
+            
+            p = RadioTap() / Dot11(addr1 = TARGET, addr2 = ROGUE_BSSID, addr3 = ROGUE_BSSID)
+            p /= Dot11Beacon(cap = 0x0104)
+            p /= Dot11Elt(ID=0, info=ESSID)
+            p /= Dot11Elt(ID=1, info="\x82\x84\x8b\x96\x0c\x12\x18\x24")
+            p /= Dot11Elt(ID=3, info="\x06")
+            p /= Dot11Elt(ID=5, info="\x01\x02\x00\x00")
+            sendp(p, iface=INTERFACE, count=10, inter=.1, verbose=0)
+        elif MODE == 2:
+        	
+            # PROBE REQUEST
+            print '[*] 802.11 Probe Request: SSID=[%s]' % (ESSID)
+            
+            STATION = "00:00:00:00:00:10"
+            p = RadioTap()/Dot11(type=0, subtype=4, addr1="ff:ff:ff:ff:ff:ff", addr2=STATION, addr3="ff:ff:ff:ff:ff:ff")
+            p /= Dot11Elt(ID=0, info=ESSID)
+            p /= Dot11Elt(ID=1, info="\x82\x84\x8b\x96") # All 802.11b rates
+            sendp(p, iface=INTERFACE, count=10, inter=.1, verbose=0)
         
-        '''	
-        # PROBE REQUEST
-        p = RadioTap()/Dot11(type=0, subtype=4, addr1="ff:ff:ff:ff:ff:ff", addr2=STATION, addr3="ff:ff:ff:ff:ff:ff")
-        p /= Dot11Elt(ID=0, info=ESSID)
-        p /= Dot11Elt(ID=1, info="\x82\x84\x8b\x96") # All 802.11b rates
-        sendp(p, iface=INTERFACE, count=2, inter=.2)
-        '''
         
-        # BROADCAST
-        p = RadioTap() / Dot11(addr1 = TARGET, addr2 = ROGUE_BSSID, addr3 = ROGUE_BSSID)
-        p /= Dot11Beacon(cap = 0x0104)
-        p /= Dot11Elt(ID=0, info=ESSID)
-        p /= Dot11Elt(ID=1, info="\x82\x84\x8b\x96\x0c\x12\x18\x24")
-        p /= Dot11Elt(ID=3, info="\x06")
-        p /= Dot11Elt(ID=5, info="\x01\x02\x00\x00")
-        #p /= Dot11Elt(ID=7, info="\x44\x45\x20\x01\x0d\x14")
-        #p /= Dot11Elt(ID=42, info="\x04")
-        #p /= Dot11Elt(ID=50, info="\x30\x48\x60\x6c")
-        sendp(p, iface=INTERFACE, count = 2, inter = .1)
-        
-
